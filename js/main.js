8d9ef1c1bdfe5955f7865e676e0bc994d67d4882
1Melky <116073583+1Melky@users.noreply.github.com>
2023-06-09 12:54:56 +0200

    about me img clickbaar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 // img doet raar als ik een a gebruik in html
 let click = document.querySelector('.click');
-click.addEventListener("click", )
+click.addEventListener("click", plekFunction)
+
+function plekFunction() {
+    window.location.href = `HL/index.html`
+};
+
+let click2 = document.querySelector('.click2');
+click2.addEventListener("click", plekFunction2)
+
+function plekFunction2() {
+    window.location.href = `Escape room spel - kopie/welcome.html`
+};
+
+let click3 = document.querySelector('.click3');
+click3.addEventListener("click", plekFunction3)
+
+function plekFunction3() {
+    window.location.href = `boterkaas/boter.html`
+};
